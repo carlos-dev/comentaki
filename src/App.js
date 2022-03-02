28b18a701f1b97f0ef0033b2969e6245631c9705
Carlos André <carlosandre1572@gmail.com>
2022-03-02 01:45:43 -0300

    Criando um novo item o Firebase

--- src/App.js
+++ src/App.js
@@ -1,37 +1,67 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/react-in-jsx-scope */
 import { useEffect, useState } from 'react';
 import {
-  getDatabase, ref, onValue, goOffline,
+  getDatabase, ref, onValue, goOffline, push, set,
 } from 'firebase/database';
 import './firebase';
 
 const db = getDatabase();
 
-function Comments() {
+const useDatabase = (endpoint) => {
   const [data, setData] = useState({});
 
-  const dataRef = ref(db, 'test');
+  const dataRef = ref(db, endpoint);
   useEffect(() => {
     onValue(dataRef, (snapshot) => {
       setData(snapshot.val());
     });
 
     return () => {
-      goOffline();
+      goOffline(db);
     };
-  }, []);
+  }, [endpoint]);
+
+  return data;
+};
+
+const useDatabasePush = (endpoint) => {
+  const [status, setStatus] = useState(false);
+
+  const save = (data) => {
+    push(ref(db, endpoint), data);
+  };
+
+  return [status, save];
+};
+
+function Comments({ visible }) {
+  const endpoint = visible ? 'test' : 'test/a';
+  const data = useDatabase(endpoint);
 
   return <pre>{JSON.stringify(data)}</pre>;
 }
 
 function App() {
   const [visible, setVisible] = useState(true);
+  const [status, save] = useDatabasePush('test');
 
   return (
     <div>
-      <button type="button" onClick={() => setVisible(!visible)}>Toggle</button>
-      {visible && <Comments />}
+      <button
+        type="button"
+        onClick={() => {
+          setVisible(!visible);
+          save({ a: 3, b: 4 });
+        }}
+      >
+        Toggle
+      </button>
+      status:
+      {' '}
+      {status}
+      <Comments visible={visible} />
     </div>
   );
 }
